c0e1ec3ef46912028646b8b878b4903fcc350036
Michaelxdd <michal.intel15@gmail.com>
2016-01-25 10:35:17 +0100

    RabbitMQ

---
+++ RabbitMQ/RabbitMQ/Receive.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {// tutaj tez deklarujemy kolejke, ze wzgledu na to, iż możemy rozpoczac odbnieranie nim wysłannik wyśle wiadomość
+                //chcemy miec pewnośc, że kolejka istnieje 
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                //mowimy serwerowi by dostarczył nam wiadomośc z kolejki
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                channel.BasicConsume(queue: "hello",
+                                     noAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQ/send.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+//Aby rabbit poprawnie działał musimy najpierw ściągnąc bibliotekę do RabbitMQ.Client.dll do naszego folderu roboczego /bin/ w projekcie
+//Następnie Tools->NuGet Package Manager w Visualu i w konsoli wpisujemy polecenie:
+//Install-Package RabbitMQ.Client
+//INFO: https://www.nuget.org/packages/RabbitMQ.Client
+// konsola deweloperska: C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\Shortcuts
+namespace RabbitMQ
+{
+    class send
+    {//Trorzymy połaczenie z serwerem
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            { //deklarujemy kolejke, która będzie przechowywac naszą wysłana wiadomość
+              //dodatkoow kolejka zostanie stworzona tylko wtedy jesli jej nie ma
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World"; //nasza wiadomośc jest tablicą, która mozna dowolnie modyfikować
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body:body);
+
+                Console.WriteLine("[x] Sent {0}", message);
+            }
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }//Zadziała tylko raz wysyłajac wiadomośc i przestanie działać
+    }
+}
